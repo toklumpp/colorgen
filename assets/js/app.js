fcac9560a1928599e6d9e19cb259700aed0c025e
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2025-09-01 18:59:00 +0100

    Fix copyright

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
-
+/*
+Copyright(c) 2025 Tobias Klumpp (https://www.toklumpp.net/)
+SPDX - License - Identifier: MIT
+*/
+"use strict";
 import { useState } from "https://esm.sh/react"
 import { createH1, createForm, createInput, createLabel, createP } from "./react-shortcuts.js";
 
