a62e6aaab3129bc25bdd66bf971225f3491a106f
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2025-09-01 20:45:23 +0100

    Add Luminance and Chroma

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,9 @@ import { useState } from "https://esm.sh/react"
 import { createH1, createForm, createInput, createLabel, createP } from "./react-shortcuts.js";
 
 export function App() {
-    const [hue, setHue] = useState("");
+    const [hue, setHue] = useState(0);
+    const [chroma, setChroma] = useState(0);
+    const [luminance, setLuminance] = useState(0);
 
     return (
         createForm({},
@@ -23,7 +25,26 @@ export function App() {
                     "Hue: ",
                     hue
                 )
-
+            ),
+            createLabel({},
+                "Chroma: ",
+                createInput({
+                    type: "number", value: chroma, onChange: (e) => setChroma(e.target.value)
+                }),
+                createP({},
+                    "Chroma: ",
+                    chroma
+                )
+            ),
+            createLabel({},
+                "Luminance: ",
+                createInput({
+                    type: "number", value: luminance, onChange: (e) => setLuminance(e.target.value)
+                }),
+                createP({},
+                    "Luminance: ",
+                    luminance
+                )
             )
         )
     );
